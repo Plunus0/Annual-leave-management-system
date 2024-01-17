0d3fa06c6762d272ee36b9a17b0ba69531f38edd
Dlsdn <wkddlsdn@gmail.com>
2024-01-17 10:23:08 +0900

    plus view

---
+++ src/main/resources/templates/js/notice_detail.js
@@ -0,0 +1,16 @@
+// notice_detail.js
+document.addEventListener('DOMContentLoaded', function () {
+    // Simulated data
+    const noticeData = {
+        id: 1,
+        title: '공지 1',
+        contents: '이곳에 공지사항 내용이 들어갑니다.\n\n\n\n\n\n\n\n 감사합니다 ',
+    };
+
+    const noticeTitle = document.getElementById('noticeTitle');
+    const noticeContents = document.getElementById('noticeContents');
+
+    // Set title and contents
+    noticeTitle.textContent = noticeData.title;
+    noticeContents.textContent = noticeData.contents;
+});
---
+++ src/main/resources/templates/js/script.js
@@ -0,0 +1,9 @@
+// 예제 데이터로 대시보드 카드 업데이트
+document.getElementById('totalEmployees').innerText = '100';
+document.getElementById('onAssignment').innerText = '20';
+document.getElementById('onHold').innerText = '5';
+
+document.getElementById('vacationRequests').innerText = '15';
+document.getElementById('annualLeaveRequests').innerText = '8';
+document.getElementById('halfDayRequests').innerText = '3';
+document.getElementById('sickLeaveRequests').innerText = '2';
