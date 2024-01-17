ff48e5dad1307c3e104461d1b4f9eefecc1b5a3d
Dlsdn <wkddlsdn@gmail.com>
2024-01-17 17:24:29 +0900

    today last

--- src/main/java/com/the_daul_intra/mini/controller/noticeController.java
+++ src/main/java/com/the_daul_intra/mini/controller/noticeController.java
@@ -1,25 +1,32 @@
 package com.the_daul_intra.mini.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class noticeController {
 
     @GetMapping("/notice/list")
-    public String noticeListView(){
+    public String noticeListView(Model model){
+        String message = "통신이 되나 안되나 테스트";
 
+        model.addAttribute("message", message);
 
         return "noticeList";
     }
 
-    @GetMapping("/notice/test/{id}")
-    public String noticeListTest(@PathVariable int id){
+    @GetMapping("/notice/test")
+    public String noticeListTest(@RequestParam String data, Model model){
 
-        System.out.println("데이터 송수신 : " + id);
+        System.out.println("test data 수신 : " + data);
+
 
         return "";
     }
+
+
 }
