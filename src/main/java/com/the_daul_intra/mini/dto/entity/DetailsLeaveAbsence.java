f3b983d2ba26c13e8cf70c2e275167c215acd32c
Plunus0 <ydh5527@naver.com>
2024-01-18 10:00:01 +0900

    20240118 엔티티 작성

--- src/main/java/com/the_daul_intra/mini/dto/entity/DetailsLeaveAbsence.java
+++ src/main/java/com/the_daul_intra/mini/dto/entity/DetailsLeaveAbsence.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Entity
 @AllArgsConstructor
@@ -19,6 +21,44 @@ public class DetailsLeaveAbsence {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "EMPLOYEE_ID")
+    @JoinColumn(name = "EMPLOYEE_ID", nullable = false)
     private Employee employee;
+
+    @Column(name = "START_DATE", nullable = false, length = 19)
+    private LocalDateTime startDate;
+
+    @Column(name = "END_DATE", length = 19)
+    private LocalDateTime endDate;
+
+    @Column(name = "ABSENCE_LEAVE_PERIOD", nullable = false)
+    private Integer absenceLeavePeriod;
+
+    @Column(name = "ABSENCE_TYPE", nullable = false, length = 10)
+    private String absenceType;
+
+    @Column(name = "APPLICATION_DATE", nullable = false, length = 19)
+    private LocalDateTime applicationDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "RECEPTION_ADMIN", nullable = false)
+    private Employee receptionAdmin;
+
+    @Column(name = "RECEPTION_DATE", length = 19)
+    private LocalDateTime receptionDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "PROCESSED_ADMIN", nullable = false)
+    private Employee processedAdmin;
+
+    @Column(name = "PROCESSED_DATE", length = 19)
+    private LocalDateTime processedDate;
+
+    @Column(name = "PROCESSING_STATUS", nullable = false, length = 2)
+    private String processingStatus;
+
+    @Column(name = "APPLICANT_COMMENTS", columnDefinition = "TEXT")
+    private String applicantComments;
+
+    @Column(name = "ADMIN_COMMENT", columnDefinition = "TEXT")
+    private String adminComment;
 }
--- src/main/java/com/the_daul_intra/mini/dto/entity/Project.java
+++ src/main/java/com/the_daul_intra/mini/dto/entity/Project.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Entity
 @AllArgsConstructor
@@ -19,9 +21,18 @@ public class Project {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "EMPLOYEE_ID")
+    @JoinColumn(name = "EMPLOYEE_ID", nullable = false)
     private Employee employee;
 
-    @Column(name = "PROJECT_NAME", columnDefinition = "TEXT")
+    @Column(name = "PROJECT_NAME", nullable = false, columnDefinition = "TEXT")
     private String projectName;
+
+    @Column(name = "PROJECT_STATION", columnDefinition = "TEXT")
+    private String projectStation;
+
+    @Column(name = "START_DATE", nullable = false, length = 19)
+    private LocalDateTime startDate;
+
+    @Column(name = "END_DATE", length = 19)
+    private LocalDateTime endDate;
 }
