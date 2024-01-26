21ac4dde69f6720c6eca85cc4025e51ad7b40f6e
SOOyeonP09 <psy33933@gmail.com>
2024-01-26 18:09:46 +0900

    hi

--- src/main/java/com/the_daul_intra/mini/configuration/WebSecurityConfig.java
+++ src/main/java/com/the_daul_intra/mini/configuration/WebSecurityConfig.java
@@ -1,18 +1,14 @@
 package com.the_daul_intra.mini.configuration;
 
-import com.the_daul_intra.mini.service.ApiEmpService;
 import com.the_daul_intra.mini.service.EmpDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -30,17 +26,17 @@ public class WebSecurityConfig {
                 .csrf().disable()
                 .cors().and()
                 .authorizeHttpRequests(requests -> {
-                    requests.requestMatchers(HttpMethod.POST, "/api/login", "/admin/Login").permitAll();
-                    requests.requestMatchers(HttpMethod.GET, "/admin/Login").permitAll();
-                    requests.requestMatchers("/admin/**").hasAuthority("ROLE_ADMIN");
-                    requests.anyRequest().authenticated();
-//                    requests.anyRequest().permitAll();
+//                    requests.requestMatchers(HttpMethod.POST, "/api/login", "/admin/Login").permitAll();
+//                    requests.requestMatchers(HttpMethod.GET, "/admin/Login").permitAll();
+//                    requests.requestMatchers("/admin/**").hasAuthority("ROLE_ADMIN");
+//                    requests.anyRequest().authenticated();
+                    requests.anyRequest().permitAll();
                 })
                 .sessionManagement(
                         sessionManagement ->
                                 sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                .addFilterBefore(new JwtFilter(empDetailsService, secretKey), UsernamePasswordAuthenticationFilter.class)
+//                .addFilterBefore(new JwtFilter(empDetailsService, secretKey), UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
--- src/main/java/com/the_daul_intra/mini/dto/entity/Employee.java
+++ src/main/java/com/the_daul_intra/mini/dto/entity/Employee.java
@@ -6,6 +6,7 @@ import lombok.*;
 import java.util.Set;
 
 @Getter
+@Setter
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
